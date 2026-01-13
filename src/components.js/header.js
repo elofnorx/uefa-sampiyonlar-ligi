1cd3e7f2c625c2fdbcd62d2288fb4884d2ab23e1
elofnorx <147607482+elofnorx@users.noreply.github.com>
2026-01-13 18:17:02 +0300

    Son degisiklikler ve duzeltmeler

--- src/components.js/header.js
+++ src/components.js/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Header = () => {
+function Header() {
   return (
     <header>
-      <h1>UEFA CHAMPIONS LEAGUE</h1>
-      <p className="sub-title">2025-2026 Sezonu Puan Durumu</p>
+      <h1>⚽ UEFA Champions League</h1>
+      <p className="sub-title">2025–2026 Sezonu Puan Durumu</p>
     </header>
   );
-};
+}
 
 export default Header;
